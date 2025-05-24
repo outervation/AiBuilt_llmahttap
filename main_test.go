f86729369d5e65b164c33a08831e6f20aff0b68e
Jonathan Barnard <jonathan.t.barnard@gmail.com>
2025-05-24 14:40:56 +0200

    Modified main_test.go

---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+// TestMainExists is a dummy test to ensure 'go test ./...' passes.
+// It can be expanded or removed once real tests are in place.
+func TestMainExists(t *testing.T) {
+	// This test doesn't need to do much, just exist.
+	// It confirms that the main package is testable.
+}
